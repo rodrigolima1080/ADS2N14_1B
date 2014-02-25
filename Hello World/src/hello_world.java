c4ac23a37d8fbcb224c2c9e7035eb360788af216
rodrigolima1080 <rodrigolima1903@gmail.com>
2014-02-25 00:33:02 -0300

    Hello you

--- Hello World/src/hello_world.java
+++ Hello World/src/hello_world.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class hello_world {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		
+		String nome;
+		Scanner scan = new Scanner (System.in);
+		
+		
+		
+		
+		
+		
+		System.out.println("Quem é você?");		
+		nome = scan.next();
+		
+		System.out.println("Hello " + nome);
+		
+		
+		
 	}
 
 }
